a74b98279cdca45bac970117fc307bcea467c2ee
jaxer-in <90608942+jaxer-in@users.noreply.github.com>
2022-08-01 20:27:05 +0530

    Update script.js

--- .resources/js/script.js
+++ .resources/js/script.js
@@ -1,4 +1,4 @@
-const version = "v0.0.0";
+const version = "v220723";
 const rawUrl = null;
 const githubPrefix = "https://jaxer-in.github.io/cdn";
 const cloudflarePrefix = "https://cloud-flair.com";
